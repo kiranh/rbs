0f8efe03125efa48c96f88be43e5f31fc8794a31
Srikanth <sri.jjhero@gmail.com>
2011-07-25 20:08:21 +0530

    Registration page tool tip added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def show_footer?(cont=nil, act=nil)
+    if cont || act
+      if cont == "home" && (act == "new" || act == "create")
+        return false
+      end
+    else
+      return true
+    end    
+  end
 end
