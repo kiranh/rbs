71e8c8beebbd50dfb68213e6b3bfe2043398ba2e
Srikanth <sri.jjhero@gmail.com>
2011-08-01 18:00:57 +0530

    showing footer in all pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
   def show_footer?(cont=nil, act=nil)
-    if cont || act
-      if cont == "home" && (act == "new" || act == "create")
-        return false
-      end
-    else
-      return true
-    end    
+    return true
+#    if cont || act
+#      if cont == "home" && (act == "new" || act == "create")
+#        return false
+#      end
+#    else
+#      return true
+#    end
   end
 end
