2ef8573ca4ddb51bed1aa158cd9055704c32bae7
Kiran Soumya <kiran.soumya@gmail.com>
2011-08-04 00:31:12 +0530

    fix inbox message autocomplete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def show_footer?(cont=nil, act=nil)
-    return true
+    return true if cont != "users"
     #    if cont || act
     #      if cont == "home" && (act == "new" || act == "create")
     #        return false
@@ -10,7 +10,7 @@ module ApplicationHelper
     #    end
   end
 
-  def auto_complete_result(entries, field, phrase = nil)
+  def auto_complete_search_result(entries, field, phrase = nil)
     return unless entries
     items = entries.map { |entry| content_tag(:li, phrase ? highlight(link_to(entry[field], user_path(entry[field]) ), phrase) : link_to(entry[field], user_path(entry[field]) )) }
     content_tag(:ul, items.uniq.join().html_safe)
